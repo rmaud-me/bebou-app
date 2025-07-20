b70772a9bde0ac825e18d2ba8d04a751be560f71
Maud @mihani <mihani@rmaud.me>
2025-07-20 17:57:47 +0200

    feat(home): Add card for each apps

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
 
 class HomeController extends AbstractController
 {
