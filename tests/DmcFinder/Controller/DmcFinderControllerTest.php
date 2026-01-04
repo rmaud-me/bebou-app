bd8c470c06c6422fa6a39f72d6c0d316079b34ea
Maud @mihani <mihani@rmaud.me>
2026-01-04 14:51:30 +0100

    fix(doctrine): Remove controller_resolver.auto_mapping deprecated doctrine config

--- tests/DmcFinder/Controller/DmcFinderControllerTest.php
+++ tests/DmcFinder/Controller/DmcFinderControllerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Controller;
+namespace App\Tests\DmcFinder\Controller;
 
 use App\DmcFinder\Twig\Components\RgbFinder;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
